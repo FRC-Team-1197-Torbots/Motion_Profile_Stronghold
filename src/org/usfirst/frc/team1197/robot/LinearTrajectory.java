a169700cd105cf2ced95be4795a0ecf6893a03d9
joeLoughboro <joeloughboro@gmail.com>
2017-01-03 09:22:22 -1000

    Fixed a minor bug in LinearTrajectory.build(), added comments.

--- src/org/usfirst/frc/team1197/robot/LinearTrajectory.java
+++ src/org/usfirst/frc/team1197/robot/LinearTrajectory.java
@@ -55,23 +55,40 @@ public class LinearTrajectory extends TorTrajectory{
 		return 0;
 	}
 	
+	// The following magic was adapted from 254's TrajectoryLib.
 	protected void build(){
-		double adjusted_max_acc = Math.min(max_acc, Math.pow((0.5 * goal_pos * max_jerk * max_jerk), (1.0/3.0)));
-		double adjusted_max_vel = Math.min(max_vel,
+		// This guarantees that if we don't have the runway to accelerate up to top speed
+		// or to "jerk" up to top acceleration, we set more realistic goals:
+		double disp = Math.abs(goal_pos); // Worry about sign later.
+		double adjusted_max_acc = Math.min(max_acc,
+										   Math.min(Math.sqrt(max_vel * max_jerk),
+										            Math.pow((0.5 * disp * max_jerk * max_jerk), (1.0/3.0))));
+		double adjusted_max_vel = Math.min( max_vel,
 				(-adjusted_max_acc * adjusted_max_acc + Math.sqrt(adjusted_max_acc
 						* adjusted_max_acc * adjusted_max_acc * adjusted_max_acc
-						+ 4 * max_jerk * max_jerk * adjusted_max_acc
-						* Math.abs(goal_pos))) / (2 * max_jerk));
-
-		int f0_length = (int) Math.round((TorMotionProfile.getTimeInterval() + goal_pos / adjusted_max_vel) * (1/TorMotionProfile.getTimeInterval()));
-		adjusted_max_vel = goal_pos/(f0_length * TorMotionProfile.getTimeInterval());
-		int f1_length = (int) Math.ceil((adjusted_max_vel / adjusted_max_acc) / TorMotionProfile.getTimeInterval());
-		int f2_length = (int) Math.ceil((adjusted_max_acc / max_jerk) / TorMotionProfile.getTimeInterval());
-		int time = (int) (Math.ceil(f0_length + f1_length + f2_length));
-		secondOrderFilter(f0_length, f1_length, f2_length, TorMotionProfile.getTimeInterval(), adjusted_max_vel, time);
+						+ 4 * max_jerk * max_jerk * adjusted_max_acc * disp)) 
+				/ (2 * max_jerk) );
+		
+		// Since each filter has a discrete number of slots, we need this "fancy rounding code" 
+		// to make sure the robot always drives exactly the correct distance, and always
+		// moves in a way that does not exceed the maximum velocity, acceleration, or
+		// jerk. Without this, the use of a digital filter to produce motion profiles can
+		// give unexpected results that violate these constraints, especially when
+		// the path is short or the maximum velocity, acceleration, and/or jerk are high.
+		double dt = TorMotionProfile.getTimeInterval();
+		int f0_length = (int) (1 + Math.round(disp / (adjusted_max_vel * dt)));
+		adjusted_max_vel = disp/(f0_length * dt);
+		int f1_length = (int) Math.ceil(adjusted_max_vel / (adjusted_max_acc * dt));
+		int f2_length = (int) Math.ceil(adjusted_max_acc / (max_jerk * dt));
+		int tot_length = f0_length + f1_length + f2_length;
+		secondOrderFilter(f0_length, f1_length, f2_length, dt, adjusted_max_vel, tot_length);
 	}
 	
-	protected void secondOrderFilter(int f0_length, int f1_length, int f2_length, double dt, double max_vel, int length) {
+	protected void secondOrderFilter(int f0_length, int f1_length, int f2_length, double dt, double max_vel, int tot_length) {
+		// Why no "f0"? Because the zero-filter can be equivalently implemented more
+		// simply by just feeding a constant velocity value into the first filter for
+		// the correct length of time. The "real" filters, on the other hand, MUST be 
+		// implemented as lists of numbers:
 		List<Double> f1 = new LinkedList<Double>();
 		for(int i = 0; i < f1_length; i++){
 			f1.add(new Double(0));
@@ -81,67 +98,59 @@ public class LinearTrajectory extends TorTrajectory{
 			f2.add(new Double(0));
 		}
 		
-		long t = 0;
-		
-		double pos = 0.0;
-		double vel = 0.0;
-		double acc = 0.0;
-		double jerk = 0.0;
-		
-		double last_pos = 0.0;
-		double last_vel = 0.0;
-		double last_acc = 0.0;
-		double last_jerk = 0.0;
-		
+		// Inputs and outputs of the filters:
 		double input;
 		double FL1out;
 		double FL2out;
+		// Individual data values:
+		double pos = 0.0;
+		double vel = 0.0;
+		double acc = 0.0;
+		// record previous values so we can do integration/differentiation:
+		double last_pos = 0.0;
+		double last_vel = 0.0;
 		
-		for (int i = 0; i < length; ++i) {
-			
+		for (int i = 0; i < tot_length; ++i) {
 			last_pos = pos;
 			last_vel = vel;
-			last_acc = acc;
-			last_jerk = jerk;
-			
+			// As long as the zero filter is not empty, feed-forward the target velocity:
 			if(f0_length > 0){
 				input = max_vel;
+				f0_length--; // Decrement the zero filter.
 			}
 			else{
 				input = 0.0;
 			}
-			
-			f0_length--;
-			
+
+			// Input goes at the beginning of the filter (end of the list):
 			f1.add(new Double(input));
+			// Throw away the element at the end of the filter (beginning of the list)
 			f1.remove(0);
-			
+			//  Output is the average of the elements in the first filter:
 			FL1out = average(f1);
-			
+			// Repeat this procedure for the second filter:
 			f2.add(new Double(FL1out));
 			f2.remove(0);
-			
 			FL2out = average(f2);
 			
+			// The output of the filter is velocity:
 			vel = FL2out;
 			velocity.addElement(new Double(vel));
-			
-			pos = (last_vel + vel) / 2.0 * TorMotionProfile.getTimeInterval() + last_pos;
+		    // We have to integrate to get position. This uses trapezoidal integration,
+		    // but the choice of integration strategy probably doesn't matter:
+			pos = last_pos + 0.5 * (last_vel + vel) * dt;
 			displacement.addElement(new Double(pos));
-			
+			// We have to differentiate to get acceleration:
 			acc = (vel - last_vel) / TorMotionProfile.getTimeInterval();
 			acceleration.addElement(new Double(acc));
-
-			jerk = (acc - last_acc) / TorMotionProfile.getTimeInterval();
-			
-//			time.addElement(new Long(t));
-//			t += (long) (1000 * TorMotionProfile.getTimeInterval());
 		}
 		
+		// It's later, time to worry about sign:
 		if(goal_pos < 0.0){
-			for(int i = 0; i < length; i++){
+			for(int i = 0; i < displacement.size(); i++){
 				displacement.set(i, -displacement.elementAt(i));
 				velocity.set(i, -velocity.elementAt(i));
+				acceleration.set(i, -acceleration.elementAt(i));
 			}
 		}
 	}
